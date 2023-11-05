4614f528e80d80b78c278f78be1d271cb154caef
ShariKahl <shari.kahl@gmx.de>
2023-11-05 22:09:34 +0100

    2.1 done

--- Source/Aufgabe1_4.s
+++ Source/Aufgabe1_4.s
@@ -1,8 +1,8 @@
 /*
  * Aufgabe_1_4.S
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: 16.10.2023
+ *      Author: Shari Kahl
  *
  *	Aufgabe : Maskenoperationen
  */
@@ -10,8 +10,30 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+	.equ BREAD_bm, (1<<0)
+	.equ BUTTER_bm, (1<<1)
+        .equ CHEESE_bm, (1<<2)
+        
+	.equ ORANGE_bm, (1<<4)
+	.equ BANANA_bm, (1<<5)
+        .equ KIWI_bm, (1<<6)
 
+        .equ WATER_bm, (1<<8)
+	.equ MILK_bm, (1<<9)
+        .equ FANTA_bm, (1<<10)
 
+        .equ SNACKS_bm, (BREAD_bm | BUTTER_bm | CHEESE_bm)
+	.equ FRUITS_bm, (ORANGE_bm | BANANA_bm | KIWI_bm)
+        .equ DRINKS_bm, (WATER_bm | MILK_bm | FANTA_bm)
+
+        .equ BREAKFAST_bm, (SNACKS_bm | MILK_bm | KIWI_bm)
+
+	ldr R0, =BREAKFAST_bm
+	ldr R1, =(BREAKFAST_bm | FRUITS_bm) 
+	ldr R2, =(BREAKFAST_bm | FANTA_bm) & ~MILK_bm
+	ldr R3, =(MILK_bm | FRUITS_bm | MILK_bm<<16)
+	ldr R4, =(BREAKFAST_bm & (~(MILK_bm | CHEESE_bm)))
+      
 stop:
 	nop
 	bal stop
--- Source/Aufgabe2_1.s
+++ Source/Aufgabe2_1.s
@@ -1,8 +1,8 @@
 /*
  * Aufgabe_2_1.S
  *
- *  Created on: 25.10.2023
- *      Author: Shari Kahl
+ *  Created on: <$Date>
+ *      Author: <$Name>
  *
  *	Aufgabe : 64 Bit Addition
  */
@@ -10,21 +10,10 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
-		/* Load the 64-bit values from R0-R3 */
-		ldr r4, [r0]   /* Load lower 32 bits of the first number */
-		ldr r5, [r1]   /* Load upper 32 bits of the first number */
-		ldr r6, [r2]   /* Load lower 32 bits of the second number */
-		ldr r7, [r3]   /* Load upper 32 bits of the second number */
 
-		/* Perform the 64-bit addition */
-		adds r8, r4, r6   /* Add the lower 32 bits */
-		adc r9, r5, r7   /* Add the upper 32 bits with carry from the lower addition */
-		adc r10, r10, #0  /* Add carry from the upper addition */
-
-		/* r8 and r9 now hold the result, and r10 holds any overflow from the addition */
 
 stop:
-    nop
-    bal stop
+	nop
+	bal stop
 
 .end
