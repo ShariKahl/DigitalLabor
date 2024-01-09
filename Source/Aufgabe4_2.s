a373342d6252061872fc0535cfe722b84252d586
Shari <shari.kahl@gmx.de>
2024-01-09 08:42:50 +0100

    Aufgabe4 done

--- Source/Aufgabe4_2.s
+++ Source/Aufgabe4_2.s
@@ -1,8 +1,8 @@
 /*
  * Aufgabe_4_2.S
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: 16.12.2023
+ *      Author: Shari Kahl
  *
  *	Aufgabe : Addition von zwei 8 stelligen BCD Zahlen
  */
@@ -10,9 +10,52 @@
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
 main:
+    ldr r4, =0x12345678 
+    ldr r5, =0x55555555 
 
+    bl add 
 
-stop:
-	nop
-	bal stop
+    stop:
+    nop
+    bal stop
+
+add:
+    stmfd sp!,{r4-r7}     // r4, r5, r6, r7 auf dem Stack speichern
+    mov r0, #8                  // Stellenzhler initialisieren
+    mov r1, #0x0000000f         // Stellenmaske initialisieren - 32-Bit-Zahl, die die niederwertigsten Stellen (unteren 4 Bits) markiert
+    mov r2, #0                  // Temporre Variable
+    mov r3, #0                  // Carry/berlauf initialisieren 
+    mov r8,#0                  // Rckgabewert initialisieren 
+
+loop:
+    /*r4 und r5 mit der Maske r1 bitweise verknpft, 
+    um beide niederwertige Stellen zu isolieren und in r6 bzw. r7 zu speichern*/
+
+    and r6,r1,r4                // Letzte Ziffer von ersten Zahl (r4)
+    and r7,r1,r5                // Letzte Ziffer von zweiten Zahl (r5)
+
+    add r2,r6,r7                // Summe der niederwertigen Stellen bilden in r2
+    add r2,r2,r3                // Vorhergehenden berlauf bercksichtigen, falls Carry: r3 = 1, sonst 0
+
+    mov r3, #0                  // berlauf zurcksetzen
+
+    subs r2,#10                 // Auf berlauf prfen
+    addmi r2,#10                // Falls nach subs eine negative Zahl in r2, addiere wieder 10
+
+    movpl r3,#1                 // berlauf merken, wenn Zahl negativ ist, sonst bleibt berlauf falsch
+
+    add r8,r8,r2              // Zwischenergebnis aktualisieren
+
+    mov r8,r8, ror #4         // Zahlen rotieren fr die nchsten Stellen
+    mov r4,r4, ror #4           // Rotation von ersten Zahl (r4)
+    mov r5,r5, ror #4           // Rotation von zweiten Zahl (r5)
+
+    subs r0, #1                 // Zhler dekrementieren
+
+    bne loop
+
+    ldmfd sp!,{r4,r5,r6,r7}     // r4, r5, r6, r7 von Stack holen, Linkregister von Stack holen und in dem PC Speichen was einem
+                                // Rcksprung an die Aufruferaderesse entspricht
+    bx lr 
+
 .end
--- Source/Aufgabe4_3.s
+++ Source/Aufgabe4_3.s
@@ -1,20 +1,80 @@
 /*
  * Aufgabe_2_3.S
  *
- *  Created on: <$Date>
- *      Author: <$Name>
+ *  Created on: 16.12.2023
+ *      Author: Shari Kahl
  *
  *	Aufgabe : Datenstrom Binarisieren
  */
-.text /* Specify that code goes in text segment */
-.code 32 /* Select ARM instruction set */
-.global main /* Specify global symbol */
+.data
+Datablock_1:
+    .byte 8                 // Gre des Datenblocks
+    .byte 5                 // Status des Datenblocks, processed = 5
+    .byte 0,1,2,3,4,5,6,7   // Daten im Block
+
+Datablock_2:
+    .byte 8                 // Gre des Datenblocks
+    .byte 0                 // Status des Datenblocks, raw = 0
+    .byte 1,1,1,1,1,1,6,7   // Daten im Block
+
+.text 
+.code 32 
+.global main 
 main:
 
+  ldr r1, =3                // Schwellenwert
+  ldrb r4, =0               // Status eines Datenblocks speichern
+  ldr r5, =0                // Rckgabewert  
 
+  ldr r0, =Datablock_1
+  bl getdata                // Datenblock 1 analysieren
+
+  ldr r0, =Datablock_2
+  bl getdata                // Datenblock 2 analysieren
+
+  b stop                    
+
+getdata:
+  PUSH {r4,r5,r7,r6}        // Speicherplatz fr Register sichern
+
+  ldrb r3, [r0]             // Wert des Datenblocks an Stelle r0 holen, r3 = Gre des Datenblocks
+  ldrb r6, [r0, #1]         // Wert des Datenblocks an Stelle r0+1 holen, r6 = Statusbyte
+
+  mov r2, #2                // r2 als Zhler initialisieren
+
+  // Status des Datenblocks holen - Bereits bearbeitet?
+  cmp r6, #5                // Vergleich des Statusbytes mit 5
+  bne loop                  // Falls nicht gleich, zu loop springen
+  POP {r4,r5,r7,r6}         // Register wiederherstellen
+  bx lr                     // Rckkehr zur aufrufenden Funktion
+
+loop:
+    mov r5, r5, lsl #1       // Wert = 1; Inhalt von r5 um 1 Stelle nach links verschieben
+     
+    ldrb r7, [r0,r2]         // Wert laden, r7 = Byte aus Datenblock an Stelle r2
+
+    cmp r7, r1               // Vergleich des Bytes mit Schwellenwert
+    movgt r4, #1             // Wenn Byte > Schwellenwert, r4 = 1
+    movle r4, #0             // Wenn Byte <= Schwellenwert, r4 = 0
+
+    strb r4, [r0,r2]         // Speichern von r4 im Datenblock an Stelle r2
+
+    add r2, r2, #1           // Inkrementierung des Zhlers
+
+    subs r3, r3, #1          // Reduzierung der Datenblockgre
+
+    bne loop                 // Schleife, bis alle Datenbytes bearbeitet sind
+
+    cmp r3, #0               // Prfen, ob die Datenblockgre 0 ist
+    moveq r6, #5             // Wenn gleich 0, Statusbyte auf 5 setzen
+
+    strb r6, [r0,#1]         // Speichern des neuen Statusbytes
+
+    POP {r4,r5,r7,r6}        // Register wiederherstellen
+
+    bx lr                    
 
 stop:
-	nop
-	bal stop
-
+    nop
+    bal stop                 
 .end
