3f62044fa613e75a5c665222592f117321299602
Shari <shari.kahl@gmx.de>
2024-01-09 11:07:25 +0100

    Aufgabe7.1

--- Source/Aufgabe7_1.c
+++ Source/Aufgabe7_1.c
@@ -9,38 +9,32 @@
 #include <stdint.h>
 #include "Aufgabe7_1.h"
 
-#include <stdio.h>
+#include <stdint.h>
 
-int main(void) 
-{
-  // Gegebene Konstanten
-  uint32_t CONSTANT_A = /* Wert von CONSTANT_A */;
-  uint8_t value_a = (CONSTANT_A >> 12) & 0xFF;
-  uint8_t value_b = ~CONSTANT_A;
-  uint32_t value_c = CONSTANT_A ^ 0xF0000000;
-  uint32_t value_d = CONSTANT_A | 0xFF00FF00;
+#define CONSTANT_A 0x12345678
+#define CONSTANT_LOW 0xAB
+#define CONSTANT_HIGH 0xCD
 
-  uint8_t CONSTANT_LOW = /* Wert von CONSTANT_LOW */;
-  uint8_t CONSTANT_HIGH = /* Wert von CONSTANT_HIGH */;
+int main() {
+    uint32_t A = CONSTANT_A;
+    uint8_t value_a = (A >> 16) & 0xFF;
+    uint32_t value_b = ~A;
+    uint32_t value_c = ((A & 0xF0000000) ^ 0xF0000000) | (A & 0x0FFFFFFF);
+    uint32_t value_d = (A & 0xCFFFFFFF) | 0x30000000;
 
-  // Big Endian 16 Bit
-  uint16_t packed_big_endian_16 = ((uint16_t)CONSTANT_HIGH << 8) | (uint16_t)CONSTANT_LOW;
+    uint16_t combined_8bit = ((uint16_t)CONSTANT_HIGH << 8) | CONSTANT_LOW;
+    uint32_t combined_16bit = ((uint32_t)CONSTANT_HIGH << 16) | CONSTANT_LOW;
+    uint16_t combined_little_endian = (CONSTANT_HIGH) | ((uint16_t)CONSTANT_LOW << 8);
 
-  // Big Endian 32 Bit
-  uint32_t packed_big_endian_32 = ((uint32_t)CONSTANT_HIGH << 16) | (uint32_t)CONSTANT_LOW;
+    // Printing the values for demonstration
+    printf("value_a: %02X\n", value_a);
+    printf("value_b: %08X\n", value_b);
+    printf("value_c: %08X\n", value_c);
+    printf("value_d: %08X\n", value_d);
 
-  // Little Endian 16 Bit
-  uint16_t packed_little_endian_16 = ((uint16_t)CONSTANT_LOW << 8) | (uint16_t)CONSTANT_HIGH;
+    printf("combined_8bit: %04X\n", combined_8bit);
+    printf("combined_16bit: %08X\n", combined_16bit);
+    printf("combined_little_endian: %04X\n", combined_little_endian);
 
-  // Ausgabe der Ergebnisse
-  printf("value_a: %u\n", value_a);
-  printf("value_b: %u\n", value_b);
-  printf("value_c: %u\n", value_c);
-  printf("value_d: %u\n", value_d);
-
-  printf("Big Endian 16 Bit: %u\n", packed_big_endian_16);
-  printf("Big Endian 32 Bit: %u\n", packed_big_endian_32);
-  printf("Little Endian 16 Bit: %u\n", packed_little_endian_16);
-
-  return 0;
+    return 0;
 }
